5a95447bdf1a2c1cf039a7aebf7f9e94ea3f2dfe
Rebecka Kinn <rebeckaki@getacademy.no>
2026-03-17 10:34:08 +0100

    ewfef

---
+++ dice_roller/script.js
@@ -0,0 +1,22 @@
+const dice = [
+  {
+    name: "d20",
+    maxNum: 20,
+    img: "./img/d20.png",
+  },
+  {
+    name: "d10",
+    maxNum: 10,
+    img: "./img/d10.png",
+  },
+  {
+    name: "d6",
+    maxNum: 6,
+    img: "./img/d6.png",
+  },
+  {
+    name: "d4",
+    maxNum: 4,
+    img: "./img/d4.png",
+  },
+];
