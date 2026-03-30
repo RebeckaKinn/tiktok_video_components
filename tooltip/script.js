6e9a61f8437ed2ffc3b68f6d73e595291c9d3251
Rebecka Kinn <rebeckaki@getacademy.no>
2026-03-30 12:57:43 +0200

    adding next video idea

---
+++ tooltip/script.js
@@ -0,0 +1,23 @@
+const tooltip = document.getElementById("tooltip");
+
+let mouseX = 0;
+let mouseY = 0;
+let currentX = 0;
+let currentY = 0;
+
+document.addEventListener("mousemove", (e) => {
+  mouseX = e.clientX;
+  mouseY = e.clientY;
+});
+
+function animate() {
+  currentX += (mouseX - currentX) * 0.1;
+  currentY += (mouseY - currentY) * 0.1;
+
+  tooltip.style.left = currentX + "px";
+  tooltip.style.top = currentY + "px";
+
+  requestAnimationFrame(animate);
+}
+
+animate();
